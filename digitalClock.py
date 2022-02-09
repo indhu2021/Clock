eb9ca130c0fce8ee69a22b13137ba95587b4fd1e
indhu <indhuammu@gmail.com>
2022-02-08 23:30:16 -0600

    fixed issues in python

--- digitalClock.py
+++ digitalClock.py
@@ -1,10 +1,10 @@
 from tkinter import Tk,Label
 
 
-window =Tk{}
-window.title{"Digital Clock"}
-window.geometry{"600x300"}
+window =Tk()
+window.title("Digital Clock")
+window.geometry("600x300")
 
-window.mainloop{}
+window.mainloop()
 
 
